8e1b7cee0c0a66b58c98d18e52b4c7177daf33e5
ana-saienko <saienko_ana@icloud.com>
2021-09-28 11:29:43 +0200

    I think I did the parallax

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
+var image = document.getElementsByClassName('footer__parallax-img');
+new simpleParallax(image, {
+	scale: 1.1,	
+	delay: .6,
+	transition: 'cubic-bezier(0,0,0,1)',
+	orientation: 'down'
+});
+
 var image = document.getElementsByClassName('parallax-img');
 new simpleParallax(image, {
-	// delay: .6,
-	// transition: 'cubic-bezier(0,0,0,1)',
-	// scale: 1.1
+	scale: 1.1,	
+	delay: .6,
+	transition: 'cubic-bezier(0,0,0,1)'
 });
